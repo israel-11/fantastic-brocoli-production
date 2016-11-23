684032c60b1e5efc2cef1052a7101e8fa182fad0
ICOM STUDENTS <4035b5@ece.uprm.edu>
2016-11-23 11:23:55 -0400

    Pay system missing tutors email

--- phase2/public/src/courses/controller/coursesController.js
+++ phase2/public/src/courses/controller/coursesController.js
@@ -115,7 +115,7 @@ var app = angular.module("users")
      })
    }
 
-   $scope.donate = function(){
+   $scope.donate = function(sender){
     //  swal({
     //    title: 'Send gift card?',
     //    imageUrl: 'https://cdn.shopify.com/s/files/1/0662/0785/products/e38bd83af578077b65a31424bd24d085_1024x1024.png?v=1412203835',
@@ -131,18 +131,19 @@ var app = angular.module("users")
     //      'success'
     //    )
     //  })
-        var senderName = 'Nelson';
-        var recipientName = 'Israel';
-        var recipientPhone = '7875181788';
-        var recipientEmail = 'israel.figueroa1@upr.edu';
-        var demo = 'yifti';
-        var production = 'yiftee';
-        var page = 'http://app.'+demo+'.com/api/v1/gifts/send.html?api_token=1a461040ef067dea7f40fd8ef3b2663c4&sender_name='+senderName+'&recipient_name='+recipientName+'&recipient_phone='+recipientPhone+'&recipient_email='+recipientEmail
-        swal({
-          html:'<iframe style="border: 0px; height:300px; " src="' + page + '" width="100%" height="100%"></iframe>',
-          showCancelButton : true,
-          showConfirmButton : false
-        })
+    console.log('sender:'+sender);
+        // var senderName = sender;
+        // var recipientName = receiver;
+        // var recipientPhone = '7875181788';
+        // var recipientEmail = email;
+        // var demo = 'yifti';
+        // var production = 'yiftee';
+        // var page = 'http://app.'+demo+'.com/api/v1/gifts/send.html?api_token=1a461040ef067dea7f40fd8ef3b2663c4&sender_name='+senderName+'&recipient_name='+recipientName+'&recipient_email='+recipientEmail
+        // swal({
+        //   html:'<iframe style="border: 0px; height:300px; " src="' + page + '" width="100%" height="100%"></iframe>',
+        //   showCancelButton : true,
+        //   showConfirmButton : false
+        // })
    }
 
 }]);
